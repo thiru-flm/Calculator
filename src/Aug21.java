7176e766c8782db3e0940b199f6e2cef0eb4c288
Thiru <pulgamthiru69@gmail.com>
2025-02-11 14:40:59 +0530

    first commit

---
+++ src/Aug21.java
@@ -0,0 +1,76 @@
+
+public class Aug21 {
+
+	public static void main(String[] args) {
+		int a = 1;
+   switch (a) {
+   case 1 : {
+	   System.out.println("1");
+	   }
+   case 2 : {
+	   System.out.println("2");
+   }
+   case 3 :{
+	   System.out.println("3");
+   }
+   case 4 : {
+	   System.out.println("4");
+	   
+   }
+   case 5 : {
+	   System.out.println("5");
+   }
+   case 6 : {
+	   System.out.println("6");
+   }
+   default : { System.out.println("print nothing");}
+   
+   }
+	}
+}
+   
+   
+
+	// int b = 0;
+	// if (a < 10)
+	// b = 10;
+
+	// else if (a > 20)
+	// b = 20;
+	// else if (a > 30)
+	// b = 30; --> nested if practice examples
+	// else if (a > 40)
+	// b = 40;
+	// else if (a > 50)
+	// b = 50;
+	// else
+	// b = 60;
+
+	// int b = (a > 100) && (!(a != 0)) ? 4:5; // ternary operator example
+
+	// if (!((a < b) || (b > c))) {
+
+	// if ((a>b) && (b>c))
+
+	// b = 9; --> all are if practice examples
+	// c = 10;
+	// if (a > 100)
+	// if (a < 100)
+
+	// if (a > 100) {
+	// b = 300;
+	// c = 200; // if else practice examples
+	// } else {
+	// b = 400;
+	// c = 500;
+	// }
+	// int d = a + c;
+
+	// System.out.println(a);
+	// System.out.println(b);
+	// System.out.println(c);
+	// System.out.println(d);
+
+
+
+
